d961a18d606c8a296b328313e7e9f4539034fb2b
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-11-18 21:06:07 +0530

    Update File

--- Lab Programs/areaOfTriangle.sh
+++ Lab Programs/areaOfTriangle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #areaOfTriangle.sh: Program to find area of triangle when 3 sides are given
-echo -e "Enter the length of three sides(a, b, c): \c"
+echo -e "Enter the length of three sides(a, b, c): \c "
 read a b c
 s=`echo "scale=3;($a+$b+$c)/2" | bc`
 area=`echo "sqrt(($s*($s-$a)*($s-$b)*($s-$c)))" | bc`
