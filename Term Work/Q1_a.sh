95f9e0a1948a053569d6496a1db3b4768aef6474
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-12-01 21:01:58 +0530

    Question 1_a

---
+++ Term Work/Q1_a.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Q1_a.sh: Write a shell that takes a valid directory name as an 1 argument and recursively descend all the subdirectories, finds the maximum length 
+#of any file in that hierarchy and writes this maximum value to the standard output
+for i in $* 
+do 
+	if [ -d $i ] 
+	then 
+		echo "Large filename size is" 
+		echo `ls -Rl $1 | grep "^-" | tr -s ' ' | cut -d' ' -f 5,8 | sort -n | tail -1` 
+	else 
+		echo "Not directory" 
+	fi 
+done
