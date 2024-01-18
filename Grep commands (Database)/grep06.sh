89eabccf3d6276e35b01c7f5e3c2a1496df57d1d
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-01-18 18:00:21 +0530

    Grep commands

--- Grep commands (Database)/grep06.sh
+++ Grep commands (Database)/grep06.sh
@@ -1,2 +1,2 @@
-#Script to print the names of all the directors in the database emp.txt
+#Script to print the names of all the managers in the database emp.txt
 grep -w "manager" emp.txt | cut -d'|' -f 2
