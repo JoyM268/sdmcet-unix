44450c219dac17dc638593884fc3d8b0975c0225
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-01-18 16:54:01 +0530

    Grep commands

---
+++ Grep commands/grep09.sh
@@ -0,0 +1,2 @@
+#Script to display all the employees except the director
+grep -v "director" emp.txt
---
+++ Grep commands/grep10.sh
@@ -0,0 +1,2 @@
+#Script to display the files containing the word director in the current directory
+grep -l "director" *.txt
