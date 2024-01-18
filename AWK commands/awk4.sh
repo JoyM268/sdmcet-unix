340552e5f00419350a6501932cfa6ff0bbd5ce2e
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2024-01-18 17:43:02 +0530

    Update awk4.sh

--- AWK commands/awk4.sh
+++ AWK commands/awk4.sh
@@ -1,2 +1,2 @@
-#AWK command to display database with line number for file emp.txt
-awk -F "|" '{print NR, $0}' emp.txt
+#AWK command to display columns(2, 6) with line number for file emp.txt
+awk -F "|" '{print NR, $2, $6}' emp.txt
