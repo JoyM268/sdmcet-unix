28c5984e6e234701b1979b6f4d7ed70b8683b80c
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-01-19 08:15:01 +0530

    AWK Commands

--- AWK commands/awk09.sh
+++ AWK commands/awk09.sh
@@ -1,2 +1,2 @@
-#AWK command to print the columns 2, 3, 6 only if number of columns is greater than 7
+#AWK command to print the columns 2, 3, 6 only if number of columns is greater than 7 
 awk -F"|" 'NF > 7 {print $2, $3, $6}' emp.txt
