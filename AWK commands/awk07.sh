a362645c14f5add9cde12257a50dac857972e05d
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2024-01-19 07:50:20 +0530

    Update awk07.sh

--- AWK commands/awk07.sh
+++ AWK commands/awk07.sh
@@ -1,2 +1,2 @@
-#AWK command to print data of employees whose salary is more than 8000
-awk -F"|" '{if($6 > 7500) print}' emp.txt
+#AWK command to print column 2, 3, 6 for the given range of lines(3 to 6) in database
+awk 'NR==3, NR==6{print NR, $2, $3, $6}' emp.txt
