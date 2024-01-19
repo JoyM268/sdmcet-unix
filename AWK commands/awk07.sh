82a41c0a3ac9dcca60239bd89624cac061ccf160
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2024-01-19 07:50:43 +0530

    Update awk07.sh

--- AWK commands/awk07.sh
+++ AWK commands/awk07.sh
@@ -1,2 +1,2 @@
 #AWK command to print column 2, 3, 6 for the given range of lines(3 to 6) in database
-awk 'NR==3, NR==6{print NR, $2, $3, $6}' emp.txt
+awk -F"|" 'NR==3, NR==6{print NR, $2, $3, $6}' emp.txt
